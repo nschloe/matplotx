075eb801c505d4b98d89d4653a09e8dbbcfa82b8
Nico Schlömer <nico.schloemer@gmail.com>
2022-01-30 10:43:42 +0100

    __version__

--- src/matplotx/__init__.py
+++ src/matplotx/__init__.py
@@ -1,4 +1,5 @@
 from . import styles
+from .__about__ import __version__
 from ._cli import cli
 from ._contour import contour, discontour
 from ._labels import line_labels, show_bar_values, ylabel_top
@@ -13,4 +14,5 @@ __all__ = [
     "ylabel_top",
     "show_bar_values",
     "spy",
+    "__version__",
 ]
