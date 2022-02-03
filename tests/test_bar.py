92a048ed0b2c4da3a2d71c3f7e15bc5df2a6293d
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-03 14:08:06 -0500

    added tests

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pytest
 
 import matplotx
 
@@ -13,6 +14,27 @@ def test_bar():
         matplotx.show_bar_values("{:.2f}")
         plt.title("some title")
         plt.close()
+
+
+def test_bar_horizontal():
+    """Checks that a bar chart can be created with horizontal alignment
+    """
+    with plt.style.context(matplotx.styles.dufte_bar):
+        labels = ["label 1", "label 2"]
+        vals = [2.1, 6.3]
+        ypos = range(len(vals))
+        plt.barh(ypos, vals)
+        plt.xticks(ypos, labels)
+        matplotx.show_bar_values("{:.2f}", alignment="horizontal")
+        plt.title("some title")
+        plt.close()
+
+
+def test_wrong_alignment():
+    """Checks that an error is raised when alignment has an invalid value
+    """
+    with pytest.raises(ValueError, match="Unknown alignment"):
+        matplotx.show_bar_values(alignment="coucou")
 
 
 if __name__ == "__main__":
